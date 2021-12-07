279d11aa4bc23eb07a0f1c6f91a025ffa292d43c
nitaant789 <75366725+nitaant789@users.noreply.github.com>
2021-12-07 18:58:04 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,31 @@
+img ="";
+status ="";
+objects = [];
+function preload() {
+    img = loadImage('ac.jfif');
+}
+function setup(){
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+function modelLoaded() {
+ console.log("model is loaded");
+ status = true;
+ objectDetector.detect(img, gotResults); 
+}
+function gotResults(error, results){
+if (error) {
+    console.error(error);
+}
+else{
+    console.log(results);
+    objects = results;
+}
+}
+function draw(){
+    image(img,0,0, 640, 420);
+    
+    }
+    
